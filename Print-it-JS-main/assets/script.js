c0a57b4282db0ae61e0f6501777e0a7123022b10
oana.ciuca <oana.daniela.ciuca@gmail.com>
2024-06-30 23:01:05 +0200

    Correction idex.html

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -18,5 +18,25 @@ const slides = [
 ]
 
 
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Sélectionner les boutons de navigation gauche et droite
+    //const arrowLeft = document.querySelector('.arrow_left'); // Sélectionner le bouton de la flèche gauche
+    //const arrowRight = document.querySelector('.arrow_right'); // Sélectionner le bouton de la flèche droite
+
+// Vérifier la récupération des éléments dans la console
+console.log('Flèche gauche:', arrowLeft); // Devrait afficher le bouton de la flèche gauche dans la console
+console.log('Flèche droite:', arrowRight); // Devrait afficher le bouton de la flèche droite dans la console
+
+ // Ajouter des écouteurs d'événements pour tester le clic sur les flèches
+ arrowLeft.addEventListener('click', function () {
+	console.log('Flèche gauche cliquée'); // Afficher un message dans la console pour la flèche gauche
+	alert('Flèche gauche cliquée'); // Afficher une alerte pour la flèche gauche
+});
+
+arrowRight.addEventListener('click', function () {
+	console.log('Flèche droite cliquée'); // Afficher un message dans la console pour la flèche droite
+	alert('Flèche droite cliquée'); // Afficher une alerte pour la flèche droite
+});
 
 
+})
